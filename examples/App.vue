489434e56f81f24521c00bf7e1e1a02db52e4bf6
jcc <changejian@gmail.com>
2017-09-13 20:16:52 +0800

    Updated

--- examples/App.vue
+++ examples/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container my-5">
     <a-star></a-star>
+    <foot-bar></foot-bar>
   </div>
 </template>
 
 <script>
 import AStar from './components/AStar.vue'
+import FootBar from './components/Footer'
 import './css/styles.css'
 
 export default {
   components: {
-    AStar
+    AStar,
+    FootBar,
   },
 }
 </script>
---
+++ examples/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="mt-5">
+    <div class="container text-center">
+      <p class="heart"></p>
+      <nav>
+        <a class="nav-link" href="https://github.com/jcc/weui-distpicker" target="_blank">GitHub</a>
+        <a class="nav-link" href="https://pigjian.com" target="_blank">About</a>
+        <a class="nav-link" href="https://pigjian.com/donate" target="_blank">Donate</a>
+      </nav>
+      <p style="font-size: 13px;">Hosted by <a href="https://pages.coding.me" style="font-weight: bold">Coding Pages</a></p>
+    </div>
+  </footer>
+</template>
